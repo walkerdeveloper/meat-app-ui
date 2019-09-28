d685ebdbabef7503b50554c372b64c1f574fa360
Gabriel Dias <ti.gabriel.dias@gmail.com>
2019-09-27 23:08:03 -0300

    Incluída uma instância ActivatedRoute para utilizar o snapshot e capturar o id passado na URL e utilizá-lo na chamada Http para recuperar o recurso específico e criada a propriedade restaurant para armazená-lo

--- src/app/restaurant-detail/restaurant-detail.component.ts
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RestaurantService } from 'app/restaurants/restaurant/restaurant.service';
+import { Restaurant } from 'app/restaurants/restaurant/restaurant.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'mt-restaurant-detail',
@@ -6,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantDetailComponent implements OnInit {
 
-  constructor() { }
+  restaurant: Restaurant
+  constructor(private http: RestaurantService, 
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    //console.log(this.route.snapshot)
+    this.http.restaurantById(this.route.snapshot.params['id'])
+      .subscribe(restaurant =>this.restaurant = restaurant)
   }
 
 }
